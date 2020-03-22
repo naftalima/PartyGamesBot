af01647b712e8a08c2a71ce56f56725332de7942
naftalima <npl@cin.ufpe.br>
2020-03-22 20:38:40 -0300

    started

--- echo_bot.py
+++ echo_bot.py
@@ -1,7 +1,3 @@
 import telebot
 
 bot = telebot.TeleBot("1110772599:AAHB9ndbH893kZsnp0_pMX3A00LmsiYRb_8")
-
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
