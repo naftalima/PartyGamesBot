cdaa11717a4fd9fdcafaa7df4803ed2d08434ce7
naftalima <npl@cin.ufpe.br>
2020-03-22 20:40:05 -0300

    no module telebot

--- echo_bot.py
+++ echo_bot.py
@@ -1,3 +1,13 @@
 import telebot
 
 bot = telebot.TeleBot("1110772599:AAHB9ndbH893kZsnp0_pMX3A00LmsiYRb_8")
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.polling()
