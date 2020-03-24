d85e07bcafc3c1397ca1bdf48c25099e636aa004
naftalima <npl@cin.ufpe.br>
2020-03-24 13:42:59 -0300

    /bora

--- boraBot.py
+++ boraBot.py
@@ -15,8 +15,11 @@ def send_welcome(message):
 def send_welcome(message):
 	bot.reply_to(message, "/bora - invite people to play \n /games - list of games")
 
+imageSelect = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True)  # create the image selection keyboard
+imageSelect.add('Mickey', 'Minnie')
+
 @bot.message_handler(commands=['bora'])
 def send_welcome(message):
-	bot.reply_to(message, "o que??")
-    
+	bot.reply_to(message, "jogar o que?")
+
 bot.polling()
