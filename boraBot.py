a9cb441920f95b2083c9e3c724ffe7dad326aa9c
naftalima <npl@cin.ufpe.br>
2020-03-24 15:58:38 -0300

    Not stable

--- boraBot.py
+++ boraBot.py
@@ -7,19 +7,8 @@ with open("token.txt") as f:
 
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-	bot.reply_to(message, "hi, o que vc tem interesse em jogar?")
-
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
-	bot.reply_to(message, "/bora - invite people to play \n /games - list of games")
-
-imageSelect = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True)  # create the image selection keyboard
-imageSelect.add('Mickey', 'Minnie')
-
-@bot.message_handler(commands=['bora'])
-def send_welcome(message):
-	bot.reply_to(message, "jogar o que?")
+	bot.reply_to(message, "sorry. v1.1 it's not finish")
 
 bot.polling()
