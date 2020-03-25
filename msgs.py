de5716c70445abde05f5f4b8e3933914c343f813
naftalima <npl@cin.ufpe.br>
2020-03-25 20:19:04 -0300

    get games.csv w/ pandas

--- msgs.py
+++ msgs.py
@@ -1,3 +1,25 @@
+import pandas as pd
+
+class Game:
+    def __init__(self,name,description,flag):
+        self.name = name
+        self.description = ''
+        self.flag = None
+
+df_games = pd.read_csv('data/games.csv')
+
+gamesList = []
+for row in df_games.itertuples():
+    game = Game(row.Name, row.Description, row.Flag)
+    gamesList.append(game)
+
+names = []
+for i in gamesList:
+    names.append(i.name)
+
+names.sort()
+jogos = "\n".join(names)
+
 start = (
     'Lorem Ipsum is simply dummy text of the printing'
     'Lorem Ipsum is simply dummy text of the printing'
@@ -11,10 +33,3 @@ help = (
     '/editarMeusJogos - Lorem Ipsum is simply dummy text of the printing\n'
     '/SAC - Lorem Ipsum is simply dummy text of the printing'
 )
-
-dict_jogos = {
-    'GAME1' : 'descrição 1',
-    'Game2' : 'descrição 2'
-}
-
-jogos = '\n'.join(list(dict_jogos.keys()))
