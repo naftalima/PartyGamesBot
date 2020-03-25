eb18ba3bef8ef5318cae60755ee9aed7d18e33b4
naftalima <npl@cin.ufpe.br>
2020-03-25 13:58:26 -0300

    pass

--- boraBot.py
+++ boraBot.py
@@ -9,28 +9,33 @@ with open("token.txt") as f:
 
 bot = telebot.TeleBot(TOKEN)
 
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
 	bot.reply_to(message, msgs.start)
 
 @bot.message_handler(commands=['help'])
-def send_commands(message):
+def handle_help(message):
 	bot.reply_to(message, msgs.help)
 
 @bot.message_handler(commands=['bora'])
+def handle_help(message):
 	pass
 
 @bot.message_handler(commands=['jogos'])
-def send_games(message):
+def handle_jogos(message):
 	bot.reply_to(message, msgs.jogos)
 
 @bot.message_handler(commands=['meusJogos'])
+def handle_userJogos(message):
 	pass
 
 @bot.message_handler(commands=['editarMeusJogos'])
+def handle_editUserJogos(message):
 	pass
 
 @bot.message_handler(commands=['SAC'])
+def handle_SAC(message):
 	pass
 
 bot.polling()
